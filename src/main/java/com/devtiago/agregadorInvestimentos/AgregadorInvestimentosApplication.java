2479afd813e2b83209a4dd8a3a4b4acc7d77541f
tiago <tiago.rodrigues.tr99@hotmail.com>
2025-03-31 22:01:58 -0300

    iniciando o projeto!

---
+++ src/main/java/com/devtiago/agregadorInvestimentos/AgregadorInvestimentosApplication.java
@@ -0,0 +1,12 @@
+package com.devtiago.agregadorInvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestimentosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devtiago/agregadorInvestimentos/AgregadorInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiago.agregadorInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
